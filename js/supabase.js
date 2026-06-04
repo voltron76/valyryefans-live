9283d9588d6b9cbc7b8b3d0b09e0a209362339f1
voltron76 <voltron76@users.noreply.github.com>
2026-06-04 22:46:12 +0200

    Fix supabase client resolution on GitHub pages

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,3 @@
 import { CONFIG } from './config.js';
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-export const supabase = createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_ANON_KEY);
+export const supabase = window.supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_ANON_KEY);
